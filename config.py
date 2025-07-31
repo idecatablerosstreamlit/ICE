90bfe5df73aec66037ebd2018862dac4a301d05f
YBCP <ybcaballerop@unal.edu.co>
2025-07-31 16:07:45 -0500

    Update config.py

--- config.py
+++ config.py
@@ -99,7 +99,7 @@ def create_banner():
             st.markdown(f'''
             <div style="text-align: center;">
                 <img src="data:image/png;base64,{logo_bogota}" 
-                     style="width: 500px; height: auto;" 
+                     style="width: 300px; height: auto;" 
                      alt="Datos Abiertos Bogotá">
                 <div style="font-size: 10px; font-weight: 500; margin-top: 5px;"></div>
             </div>
